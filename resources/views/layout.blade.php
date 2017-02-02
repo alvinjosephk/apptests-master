7b691982ccffd82f6deb25d2fd0d60214919e6b5
alvinjosephk <alvinkanniman@gmail.com>
2017-02-02 12:06:13 +0530

    landing page
    added a nav bar with sign in and login

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
  <html lang="en">
  <head>
  <meta charset="UTF-8">
- <title>MINI PROJECT INTRO APP </title>
+ <title>OSIRES </title>
  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
  <script src="https://code.jquery.com/jquery.js"></script>
  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
@@ -10,7 +10,7 @@
  </head>
  <body>
  <header>
- <nav class="navbar navbar-default" role="navigation">
+ <nav class="navbar navbar-inverse" role="navigation">
  <!-- Brand and toggle get grouped for better mobile display -->
  <div class="navbar-header">
  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -20,21 +20,29 @@
  <span class="icon-bar"></span>
 
  </button>
- <a class="navbar-brand" href="/">INRO APPLICATION</a>
+ <a class="navbar-brand" href="/">OSIRES</a>
  </div>
 
  <!-- Collect the nav links, forms, and other content for toggling -->
  <div class="collapse navbar-collapse" id ="bs-example-navbar-collapse-1">
 
  <ul class="nav navbar-nav navbar-right">
- <li><a href="/">Home</a></li>
- <li><a href="/about">About</a></li>
- <li><a href="/contact">Contact</a></li>
- <li><a href="/get_sim">Get My SIM</a></li>
+   <li><a href="/register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
  </ul>
  </div><!-- /.navbar-collapse -->
  </nav>
  </header>
+
+
+
+ <img src="img/osires.jpg" alt="" style="width:100%;height:"">
+
+<div class="container">
+ <footer>developed by             Adarsh S   Alvin Joseph K  Dimal D Joseph  Dhanya T</footer>
+ </div>
+
+
 
  @yield('content')
  </body>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('layout');
+});
 Route::get('/in', function () {
     return view('insertForm');
 });
