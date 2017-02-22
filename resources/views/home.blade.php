98b2ec23254ee2340e6684442b63d638630f4889
alvinjosephk <alvinkanniman@gmail.com>
2017-02-22 18:14:52 +0530

    sim

---
+++ resources/views/home.blade.php
@@ -0,0 +1,77 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+<<<<<<< HEAD
+                  </div>
+<div class="panel-body">
+ <center>
+     Choose your SIM
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <a href="/choose_sim" class="thumbnail">
+        <img src="img/idea.jpg" alt="IDEA SIM">
+      </a>
+    </div>
+      <div class="col-sm-6 col-md-4">
+        <a href="/choose_sim" class="thumbnail">
+          <img src="img/airtel.jpg" alt="AIRTEL">
+        </a>
+      </div>
+    <div class="col-sm-7 col-md-4">
+      <a href="/choose_sim" class="thumbnail">
+        <img src="img/bsnl.jpg" alt="BSNL SIM">
+      </a>
+    </div>
+</div>
+</center>
+
+</div>
+=======
+                    You are logged in!
+                </div>
+
+                <div class="panel-body">
+                 <center>
+                     Choose your SIM
+                  <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                      <a href="/choose_sim" class="thumbnail">
+                        <img src="img/idea.jpg" alt="IDEA SIM">
+                      </a>
+                    </div>
+                      <div class="col-sm-6 col-md-4">
+                        <a href="/choose_sim" class="thumbnail">
+                          <img src="img/airtel.jpg" alt="AIRTEL">
+                        </a>
+                      </div>
+                    <div class="col-sm-7 col-md-4">
+                      <a href="/choose_sim" class="thumbnail">
+                        <img src="img/airtel.jpg" alt="BSNL SIM">
+                      </a>
+                    </div>
+                </div>
+                </center>
+
+                </div>
+>>>>>>> origin/master
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
