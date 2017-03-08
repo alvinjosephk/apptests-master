050eb97da5a0e6bb276cbd67cd22a74dbca74799
alvinjosephk <alvinkanniman@gmail.com>
2017-03-08 08:14:53 +0530

    customer

---
+++ database/migrations/2017_03_07_185422_create_cus4_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCus4Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('customer',function(Blueprint $table){
+
+        $table->bigincrements('reg-id')->primary;
+    //   $table->integer('id');
+    //   $table->foreign('id')->references('id')->on('users');
+        $table->string('name');
+        $table->date('DoB');
+        $table->char('gender');
+        $table->string('house-no');
+        $table->string('street');
+        $table->string('locality');
+        $table->string('city');
+        $table->string('district');
+        $table->string('state');
+      //  $table->string('email');
+
+        $table->string('nationality');
+        $table->bigInteger('aadhaar');
+
+      });
+      DB::update("ALTER TABLE customer AUTO_INCREMENT=7777;");
+  //  DB::ALTER Table ('cutomer') ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  //  ADD INDEX ('id'//
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
